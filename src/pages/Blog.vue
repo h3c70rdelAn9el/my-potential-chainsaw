3f015f0db39d96bf156a4ee9031fff90a15652ad
Hector del Angel <hectordelangel@gmail.com>
2020-12-09 23:15:42 -0800

    Quick typo fix

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -3,7 +3,7 @@
     <div>
       <h1 class="text-6xl text-center font-title -mb-8 -mt-2">Blog</h1>
     </div>
-    <div class="content md:flex ">
+    <div class="content md:flex">
       <div class="mx-6 mt-3 md:mx-2 md:mb-2 p-2 shadow-xl border border-gray md:w-1/3 md:mt-4 md:p-4 text-sm ">
         <p>Welcome to my personal "almost" blog.  These are mostly my projects, and not really written for anyone else to gain any benefit from it besides myself.  Just a way to upload and jot my learnings and failures.  I will also (in the future) try to include my other interests, again, only really beneficial to me. </p> 
       </div>
