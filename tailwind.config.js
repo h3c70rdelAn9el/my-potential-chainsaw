fbf77a5dc37f752728afa1e16f8ec66d097fbdd1
Hector del Angel <hectordelangel@gmail.com>
2020-12-10 13:58:47 -0800

    Update some tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,35 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: [
+      './src/**/*.vue',
+      './src/**/*.js',
+      './src/**/*.jsx',
+      './src/**/*.html',
+      './src/**/*.pug',
+      './src/**/*.md',
+      './docs/**/*.md',
+      './blog/**/*.md',
+    ],
+    options: {
+      whitelist: [
+        'body',
+        'html',
+        'img',
+        'a',
+        'g-image',
+        'g-image--lazy',
+        'g-image--loaded',
+        'active',
+      ],
+    },
+  },
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Raleway", "sans-serif"],
-        title: ["Peddana", "serif"],
-        logo: ["Cascadia", "serif"]
-      }
+        sans: ['Raleway', 'sans-serif'],
+        title: ['Peddana', 'serif'],
+        logo: ['Cascadia', 'serif'],
+      },
     },
   },
   variants: {},
